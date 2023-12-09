08635360a73500d74dab80d92390cbafeb947925
Kushagra Gupta <kushagragupta@Kushagras-MacBook-Air.local>
2023-12-10 02:21:35 +0530

    Final

--- server/index.js
+++ server/index.js
@@ -3,24 +3,24 @@ const { exec } = require('child_process');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = 3003;
 
 app.use(express.static('public'));
 
 app.post('/execute', (req, res) => {
   // Specify the path to your npm project folder
-  const npmProjectPath = '../';
+  const npmProjectPath = '../GeneticDataSharing/contract/contracts';
 
   // Change to the npm project folder
   process.chdir(npmProjectPath);
 
   // Execute the desired npm command (e.g., start)
-  exec('a', (error, stdout, stderr) => {
+  exec('forge create contracts/Geneticdatasharing.sol:GeneticDataSharing --constructor-args 0 --rpc-url https://sepolia-rpc.scroll.io --verify --private-key 7aa9382df0475e42859cff4b752dde4a71dcc7d6111ac813c13e1ba74d430fca', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing npm command: ${error.message}`);
       return res.status(500).send(`Error executing npm command: ${error.message}`);
     }
-    console.log(`npm command output: ${stdout}`);
+    console.log(`npm command script output: ${stdout}`);
     res.status(200).send('Execution initiated');
   });
 });
